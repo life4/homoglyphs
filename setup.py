81e729de8c812d594aec1de70ba32e98ec2a8f4b
Gram <master_fess@mail.ru>
2020-05-09 07:28:55 +0200

    upd setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='homoglyphs',
-    version='2.0.0',
+    version='2.0.2',
     description='Homoglyphs',
     python_requires='>=3.5',
     project_urls={"homepage": "https://github.com/life4/homoglyphs"},
